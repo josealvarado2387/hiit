636ae05ce1669ec62278e23245df4aeaa520d335
josealvarado2387 <joselalvarado.c2387@gmail.com>
2021-01-30 22:20:05 +0000

    First

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Connection URL
+const url = 'mongodb+srv://jose:Ireland17@cluster0.fa7wx.mongodb.net/?retryWrites=true&w=majority';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// Database Name
+const dbName = 'ca1cbwa2020092';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
 });
+function createTextIndex(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('bodytype');
+    // Create the index
+    collection.createIndex(
+      { comments : "Results" }, function(err, result) {
+      console.log(result);
+      callback(result);
+    });
+  };
